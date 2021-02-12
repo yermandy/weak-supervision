c7eaabaadd54eaeb1fa0d2ae8c0a964b10d66ab1
Andy Yermakov <yermaand@gmail.com>
2021-02-12 14:52:03 +0100

    median visualization

---
+++ sandbox/canvas.py
@@ -0,0 +1,93 @@
+import os, sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import matplotlib
+import numpy as np
+matplotlib.use("Qt5Agg")
+from PyQt5 import QtWidgets, QtGui
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+from milp import milp
+from sandbox import Point
+
+class Canvas(FigureCanvas):
+    """A canvas that updates itself every second with a new plot."""
+
+    def __init__(self, X, K, parent=None, width=6, height=6):
+
+        self.fig = Figure(figsize=(width, height))
+        self.axes = self.fig.add_subplot(111)
+
+        self.axes.grid(False)
+        self.fig.tight_layout()
+
+        FigureCanvas.__init__(self, self.fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+        self.points = []
+        self.colors = {1: 'r', 2: 'g', 3: 'b'}
+        self.K = K
+
+        self.show()
+        
+        self.median = Point(self, 0.5, 0.5, 'black', draggable=False)
+        self.milp_median = Point(self, 0.5, 0.5, 'magenta', draggable=False)
+        self.plot_draggable_points(X, K)
+        
+
+
+    def update_median(self):
+        X = []
+        for p in self.points:
+            x, y = p.point.center
+            X.append([x, y])
+        X = np.array(X)
+
+        median = np.median(X, axis=0)
+        milp_median = milp(X, self.K, False)
+        
+        self.median.point.center = median
+        self.milp_median.point.center = milp_median
+
+
+    def plot_draggable_points(self, X, K):
+        """Plot and define the 2 draggable points of the baseline"""
+        
+        for (x, y), k in zip(X, K):
+            point = Point(self, x, y, self.colors[k])
+            self.points.append(point)
+
+        self.update_median()
+        self.update_figure()
+
+
+    def clear_figure(self):
+        """Clear the graph"""
+
+        self.axes.clear()
+        self.axes.grid(False)
+        del(self.points[:])
+        self.update_figure()
+
+
+    def update_figure(self):
+        """Update the graph. Necessary, to call after each plot"""
+
+        self.draw()
+
+
+if __name__ == '__main__':
+
+    X = []
+    for i in range(6):
+        X.append([0.5,0.5])
+    K = [1,1,2,2,3,3]
+
+    app = QtWidgets.QApplication(sys.argv)
+    Canvas(X, K)
+    sys.exit(app.exec_())
