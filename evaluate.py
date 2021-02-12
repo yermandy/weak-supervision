25484cd540f8931f0d5c2bb6791c0d5e1f3557ca
Andy Yermakov <yermaand@gmail.com>
2021-02-12 11:01:21 +0100

    evaluate

--- evaluate.py
+++ evaluate.py
@@ -1,64 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def calc_prec_recall(y_true, distances):
-
-    distances = np.array(distances)
-    min_d = np.min(distances) + 1e-1
-    max_d = np.max(distances)
-    thresholds = np.linspace(min_d, max_d, min(100, len(distances)))
-
-    recall, prec = [], []
-
-    for th in thresholds:
-
-        y_pred = np.where(distances <= th, 1, 0)
-
-        tp = np.sum((y_true == y_pred) & (y_pred == 1))
-        fp = np.sum((y_pred == 1) & (y_true != y_pred))
-        fn = np.sum((y_pred == 0) & (y_true != y_pred))
-
-        x = tp / (tp + fn + 1e-8)
-        y = tp / (tp + fp + 1e-8)
-
-        recall.append(x)
-        prec.append(y)
-
-    return recall, prec
+from numpy.linalg import norm
+from milp import milp
+from metrics import calc_prec_recall
+from plots import plot_prec_recall
+from parse import *
 
 
-def plot_prec_recall(recall, prec):
-    plt.xlabel("recall")
-    plt.ylabel("precision")
-    plt.plot(recall, prec)
-    plt.xlim(0, 1)
-    plt.ylim(0, 1)
-    plt.show()
-
-
-def filter_by_counts(metadata, min_counts=0, max_counts=np.inf):
-
-    paths = metadata[:, 0]
-    subjects = metadata[:, 6].astype(int)
-
-    _, indices, counts = np.unique(
-        [paths, subjects], return_index=True, return_counts=True, axis=1
-    )
-
-    only_idx = (counts > min_counts) & (counts < max_counts)
-
-    indices = indices[only_idx]
-    counts = counts[only_idx]
+def filter_by_counts(subj_bag_indices, min_counts=1, max_counts=np.inf):
 
     filtered = []
+    for bag_indices in subj_bag_indices.values():
+        
+        for indices in bag_indices.values():
 
-    for idx, cnt in zip(indices, counts):
+            indices_len = len(indices)
 
-        for i in range(cnt):
-            filtered.append(idx + i)
+            if min_counts <= indices_len <= max_counts:
+                filtered.extend(list(indices))
 
     return filtered
+
+def predict(bag_to_index, m, dist, X, mu):
+    y_pred = np.zeros(m)
+
+    objective = []
+
+    for indices in bag_to_index.values():
+        argmin = dist[indices].argmin()
+        pred_id = indices[argmin]
+        y_pred[pred_id] = 1
+        
+        d = mu - X[indices].T
+        objective.append(np.abs(d).sum(axis=0).min())
+    
+    objective = np.mean(objective)
+
+    return y_pred, objective
 
 
 if __name__ == "__main__":
@@ -67,7 +46,9 @@ if __name__ == "__main__":
     metadata_file = "resources/ijbb_metadata.csv"
     metadata = np.genfromtxt(metadata_file, dtype=str, delimiter=",")
 
-    indices = filter_by_counts(metadata, 0)
+    subj_bag_indices = parse_metadata(metadata)
+
+    indices = filter_by_counts(subj_bag_indices, 2)
 
     metadata = metadata[indices]
     features = features[indices]
@@ -76,62 +57,128 @@ if __name__ == "__main__":
     subjects = metadata[:, 6].astype(int)
     ground_true = metadata[:, 7].astype(int)
 
-    tp = {"baseline_1": 0, "baseline_2": 0}
-    fp = {"baseline_1": 0, "baseline_2": 0}
-    fn = {"baseline_1": 0, "baseline_2": 0}
+    dists = {}
+    preds = {}
+    objs = {}
 
-    # true_correct_matches = np.sum(ground_true == 1)
+    baselines = ["baseline_*", "baseline_1", "baseline_2", "baseline_3", "baseline_4"]
+    for b in baselines:
+        dists[b] = []
+        preds[b] = []
+        objs[b] = []
 
-    distances = []
     y_true = []
+    # sorted_subjects = []
 
-    for subject in np.unique(subjects):
+    # subjects_counter = 0
+    counter = 0
+    for s, subject in enumerate(np.unique(subjects)):
 
         idx = np.flatnonzero(subjects == subject)
-        k = len(np.unique(paths[idx]))
-        ones_k = np.ones(k)
+        
+        m = len(idx)
+        if m >= 7:
+            continue
+
+        counter += 1
+        print(counter)
+
+        paths_subset = paths[idx]
         features_subset = features[idx]
-        true_subset = ground_true[idx]
+        labels_subset = ground_true[idx]
+        
+        y_true.extend(labels_subset)
 
-        # baseline 1: median
+        unique_paths, bag_to_index, index_to_bag = parse_paths(paths_subset)
+        img_per_subj = len(unique_paths)
 
-        median = np.median(features_subset, axis=0, keepdims=True).T
-        cos_dist = (1 - features_subset @ median).flatten()
-        arg_sorted = cos_dist.argsort()
-        arg_sorted = arg_sorted[:k]
-        true_k = true_subset[arg_sorted]
-        tp["baseline_1"] += np.sum(true_k == 1)
-        fp["baseline_1"] += np.sum((true_k == 1) & (ones_k != true_k))
-        fn["baseline_1"] += np.sum((true_k == 0) & (ones_k != true_k))
+        # sorted_subjects.append(np.arange(subjects_counter, subjects_counter + m))
+        # subjects_counter += m
 
-        # TODO whole array or just [0] element?
-        d = cos_dist[arg_sorted][0]
-        distances.append(d)
-        y = true_k[0]
-        y_true.append(y)
+        # Baseline *: Mean
 
-        # baseline 2: two pass median
+        mu = np.mean(features_subset[labels_subset.astype(bool)], axis=0, keepdims=True).T
+        mu_normalized = mu / norm(mu)
+        cos_dist = (1 - features_subset @ mu_normalized).flatten()
 
-        median = np.median(features_subset[arg_sorted], axis=0, keepdims=True).T
-        cos_dist = (1 - features_subset @ median).flatten()
-        arg_sorted = cos_dist.argsort()
-        arg_sorted = arg_sorted[:k]
-        true_k = true_subset[arg_sorted]
-        tp["baseline_2"] += np.sum(true_k == 1)
-        fp["baseline_2"] += np.sum((true_k == 1) & (ones_k != true_k))
-        fn["baseline_2"] += np.sum((true_k == 0) & (ones_k != true_k))
+        y_pred, objective = predict(bag_to_index, m, cos_dist, features_subset, mu)
 
-    recall, prec = calc_prec_recall(y_true, distances)
+        b = 'baseline_*'
+        dists[b].extend(cos_dist)
+        preds[b].extend(y_pred)
+        objs[b].append(objective)
+        
+        # Baseline 1: Median
+        
+        mu = np.median(features_subset, axis=0, keepdims=True).T
+        mu_normalized = mu / norm(mu)
+        cos_dist = (1 - features_subset @ mu_normalized).flatten()
 
-    plot_prec_recall(recall, prec)
+        y_pred, objective = predict(bag_to_index, m, cos_dist, features_subset, mu)
 
-    recall = tp["baseline_1"] / (tp["baseline_1"] + fn["baseline_1"] + 1e-8)
-    precision = tp["baseline_1"] / (tp["baseline_1"] + fp["baseline_1"] + 1e-8)
-    print("baseline 1")
-    print(recall, precision)
-    print()
+        b = 'baseline_1'
+        dists[b].extend(cos_dist)
+        preds[b].extend(y_pred)
+        objs[b].append(objective)
 
-    recall = tp["baseline_2"] / (tp["baseline_2"] + fn["baseline_2"] + 1e-8)
-    precision = tp["baseline_2"] / (tp["baseline_2"] + fp["baseline_2"] + 1e-8)
-    print("baseline 2")
-    print(recall, precision)
+        y_pred_baseline_1 = y_pred
+
+        # Baseline 2: Two Pass Median
+
+        mu = np.median(features_subset[y_pred_baseline_1.astype(bool)], axis=0, keepdims=True).T
+        mu_normalized = mu / norm(mu)
+        cos_dist = (1 - features_subset @ mu_normalized).flatten()
+
+        y_pred, objective = predict(bag_to_index, m, cos_dist, features_subset, mu)
+
+        b = 'baseline_2'
+        dists[b].extend(cos_dist)
+        preds[b].extend(y_pred)
+        objs[b].append(objective)
+
+        # Baseline 3: Average of Medians
+
+        mu = np.mean(features_subset[y_pred_baseline_1.astype(bool)], axis=0, keepdims=True).T
+        mu_normalized = mu / norm(mu)
+        cos_dist = (1 - features_subset @ mu_normalized).flatten()
+
+        y_pred, objective = predict(bag_to_index, m, cos_dist, features_subset, mu)
+
+        b = 'baseline_3'
+        dists[b].extend(cos_dist)
+        preds[b].extend(y_pred)
+        objs[b].append(objective)
+
+        # Baseline 4: MILP
+
+        '''
+        K = list(index_to_bag.values())
+        mu, milp_obj_val = milp(features_subset, K, False, True)
+
+        mu = np.atleast_2d(mu).T
+        mu_normalized = mu / norm(mu)
+
+        cos_dist = (1 - features_subset @ mu_normalized).flatten()
+
+        y_pred, objective = predict(bag_to_index, m, cos_dist, features_subset, mu)
+
+        b = 'baseline_4'
+        dists[b].extend(cos_dist)
+        preds[b].extend(y_pred)
+        objs[b].append(milp_obj_val / img_per_subj)
+        # '''
+
+    # generate plots
+
+    for b in baselines:
+        label = b.replace('_', ' ')
+
+        if len(preds[b]) == 0:
+            continue
+
+        recall, prec = calc_prec_recall(y_true, preds[b], dists[b])
+        plt.plot(recall, prec, label=label)
+
+        print(f'{label}: {np.mean(objs[b]):.6f}')
+
+    plot_prec_recall()
