f105d4b72be54e12bf0c87a54bf38255906ac7c3
Andy Yermakov <yermaand@gmail.com>
2021-04-09 13:57:41 +0200

    refactor

--- evaluate_ijbb.py
+++ evaluate_ijbb.py
@@ -4,157 +4,203 @@ from time import time
 
 
 if __name__ == "__main__":
-    features_file = "resources/features/ijbb_features.npy"
+
     metadata_file = "resources/ijbb_metadata.csv"
+    # features_file = "resources/features/ijbb_features.npy"
+    features_file = "resources/features/ijbb_arcface_features.npy"
 
-    paths, subjects, labels, scores, features = open_and_parse(metadata_file, features_file)
+    paths, boxes, scores, subjects, labels, features = open_and_parse(metadata_file, features_file, 1)
 
-    evaluator = Evaluator()
+    indices_img = np.flatnonzero(np.core.defchararray.find(paths , 'img') != -1)
+    indices_frames = np.flatnonzero(np.core.defchararray.find(paths , 'frames') != -1)
+
+    ijbb = np.genfromtxt('resources/ijbb_faces.csv', dtype=str, delimiter=",")
+    ijbb_paths = ijbb[:, 0]
+    ijbb_subjects = ijbb[:, 6].astype(int)
+
+    ijbb_features_optimal = np.ones((len(ijbb), features.shape[1]))
+    ijbb_features_median = np.ones((len(ijbb), features.shape[1]))
+    ijbb_features_iterative_median = np.ones((len(ijbb), features.shape[1]))
+    ijbb_features_bag_median = np.ones((len(ijbb), features.shape[1]))
+    ijbb_features_bayes_median = np.ones((len(ijbb), features.shape[1]))
+
+    bayes_distances = []
+    bayes_predictions = []
+    skipped_subjects = []
+
+    eval_optimal_median = 1
+    eval_median = 1
+    eval_iterative_median = 1
+    eval_bayes_median = 1
+    eval_milp_median = 0
+    eval_bag_median = 1
+
+    train_only_on_images = 0
+    test_only_on_images = 0
+    test_only_on_frames = 0
+
+    remove_single = 1
+    save_features = 1
+    pca_reduction = 0
+    filter_by_quality = 0
 
     counter = 0
+
+    evaluator = Evaluator()
     for s, subject in enumerate(np.unique(subjects)):
 
         idx = np.flatnonzero(subjects == subject)
+        idx_tst = idx.copy()
+        idx_trn = idx.copy()
 
-        scores_subset = scores[idx]
-        mask = scores_subset >= 0.0
-        idx = idx[mask]
-
-        m = len(idx)
+        if train_only_on_images:
+            idx_trn = np.intersect1d(idx, indices_img)
+        if test_only_on_frames:
+            idx_tst = np.intersect1d(idx, indices_frames)
+        if test_only_on_images:
+            idx_tst = np.intersect1d(idx, indices_img)
         
-        if m <= 1:
-            continue
-        
-        # if m >= 30:
+        # m = len(idx_trn)
+        # if m <= 1 or m >=50:
         #     continue
 
-        paths_subset = paths[idx]
-        features_subset = features[idx]
-        labels_subset = labels[idx]
+        if remove_single:
+            _tmp = []
+            bag_to_index = parse_paths(paths[idx_trn])[1]
+            for k, v in bag_to_index.items():
+                if len(v) == 1:
+                    continue
+                _tmp.extend(v)
 
-        unique_paths, bag_to_index, index_to_bag = parse_paths(paths_subset)
-        bags_number = len(unique_paths)
+            if len(_tmp) <= 1:
+                skipped_subjects.append(subject)
+                continue
 
-        if bags_number == 1:
+            idx_trn = idx_trn[np.array(_tmp)]
+
+        if filter_by_quality:
+            scores_trn_subset = scores[idx_trn]
+            mask = scores_trn_subset >= 0.0
+            idx_trn = idx_trn[mask]
+
+        scores_trn_subset = scores[idx_trn]
+        paths_trn_subset = paths[idx_trn]
+        features_trn_subset = features[idx_trn]
+        labels_trn_subset = labels[idx_trn]
+
+        labels_tst_subset = labels[idx_tst]
+        paths_tst_subset = paths[idx_tst]
+        features_tst_subset = features[idx_tst]
+
+        unique_paths_trn, bag_to_index_trn, index_to_bag_trn = parse_paths(paths_trn_subset)
+        unique_paths_tst, bag_to_index_tst, index_to_bag_tst = parse_paths(paths_tst_subset)        
+        
+        bags_tst_number = len(unique_paths_tst)
+        bags_trn_number = len(unique_paths_trn)
+
+        if min(bags_tst_number, bags_trn_number) <= 1:
+            skipped_subjects.append(subject)
             continue
 
         counter += 1
         print(counter)
 
-        evaluator.add_true(labels_subset)
+        evaluator.add_true(labels_tst_subset)
+
+        if save_features:
+            def add_features(predictions, ijbb_features):
+                predictions = np.array(predictions, bool)
+                paths_for_ijbb = paths_tst_subset[predictions]
+                features_for_ijbb = features_tst_subset[predictions]
+
+                for f, p in zip(features_for_ijbb, paths_for_ijbb):
+                    ijbb_idx = np.flatnonzero((ijbb_paths == p) & (ijbb_subjects == subject))
+                    ijbb_features[ijbb_idx] = f
+
+        bag_trn = np.array(list(index_to_bag_trn.values()))
+        bag_tst = np.array(list(index_to_bag_tst.values()))
 
         #! PCA reduction of features
-        ''' 
-        n_components = min(len(features_subset), 2)
-        pca = PCA(n_components)
-        features_subset = pca.fit_transform(features_subset)
-        # '''
+        if pca_reduction:
+            n_components = min(len(features_trn_subset), 2)
+            pca = PCA(n_components)
+            features_trn_subset = pca.fit_transform(features_trn_subset)
 
-        #! Best possible median
-        # '''
-        mu = median(features_subset[labels_subset.astype(bool)])
-        evaluator.update('optimal', features_subset, mu, bag_to_index)
-        # '''
+        #! Median from grouind true labels
+        if eval_optimal_median:
+            mu = median(features_trn_subset[labels_trn_subset.astype(bool)])
+            distances, predictions, objective = evaluator.update('optimal', features_tst_subset, mu, bag_to_index_tst)
+            if save_features:
+                add_features(predictions, ijbb_features_optimal)
         
         #! Median
-        # '''
-        mu = median(features_subset)
-        distances, predictions_method_1, objective = evaluator.update('median', features_subset, mu, bag_to_index)
-        # '''
+        if eval_median:
+            mu = median(features_trn_subset)
+            distances, predictions, objective = evaluator.update('median', features_tst_subset, mu, bag_to_index_tst)
+            if save_features:
+                add_features(predictions, ijbb_features_median)
 
-        #! Two-Stage Median
-        # '''
-        mu = median(features_subset[predictions_method_1.astype(bool)])
-        evaluator.update('two-pass', features_subset, mu, bag_to_index)
-        # '''
+        #! Iterative median
+        if eval_iterative_median:
+            preds = evaluator.update('_', features_trn_subset, mu, bag_to_index_trn)[1]
+            mu = median(features_trn_subset)
+            mu = iterative_median(features_trn_subset, mu, preds, bag_to_index_trn)
+            distances, predictions, objective = evaluator.update('iterative', features_tst_subset, mu, bag_to_index_tst)
+            if save_features:
+                add_features(predictions, ijbb_features_iterative_median)
 
-        #! Average of Medians
-        '''
-        mu = mean(features_subset[predictions_method_1.astype(bool)])
-        evaluator.update('method_3', features_subset, mu, bag_to_index)
-        # '''
+        #! Bayes median
+        if eval_bayes_median:
+            bag_trn = np.array(list(index_to_bag_trn.values()))
+            bag_tst = np.array(list(index_to_bag_tst.values()))
 
-        #! Optimal median
-        '''
-        mu, features_reduced = optimal_median(features_subset, list(index_to_bag.values()))
-        distances, predictions, objective = evaluator.update('method_4', features_reduced, mu, bag_to_index)
-        # '''
+            bag_tst = bag_tst + np.max(bag_trn) + 1
 
-        #! Suboptimal median without PCA
-        # '''
-        # mu, features_reduced = suboptimal_median(features_subset, list(index_to_bag.values()))
-        # distances, predictions, objective = evaluator.update('no pca', features_subset, mu, bag_to_index)
+            trn_example = np.concatenate([np.ones(len(bag_trn)), np.zeros(len(bag_tst))])
+            
+            bag = np.concatenate([bag_trn, bag_tst])
 
-        #! Suboptimal median with normalized features
-        # '''
-        # features_normalized = (features_subset - features_subset.mean(axis=0)) / features_subset.var(axis=0)
-        # mu, features_reduced = suboptimal_median(features_normalized, list(index_to_bag.values()))
-        # distances, predictions, objective = evaluator.update('normalized', features_normalized, mu, bag_to_index)
-        # '''
+            features_trn_tst = np.concatenate([features_trn_subset, features_tst_subset]).T
 
-        #! Different quality thresholds
-        '''
-        mu, features_reduced = suboptimal_median(features_subset, list(index_to_bag.values()))
-        distances, predictions, objective = evaluator.update('th 0', features_subset, mu, bag_to_index)
-        
-        threshold = 0.25
-        mask = scores_subset >= threshold
-        features_thresholded = features_subset[mask]
-        K = np.array(list(index_to_bag.values()))
-        K = K[mask]
-        mu, features_reduced = suboptimal_median(features_thresholded, K)
-        distances, predictions, objective = evaluator.update('th 0.25', features_subset, mu, bag_to_index)
+            _, S, P = bayes_learn(features_trn_tst, bag, trn_example=trn_example)
 
-        threshold = 0.5
-        mask = scores_subset >= threshold
-        features_thresholded = features_subset[mask]
-        K = np.array(list(index_to_bag.values()))
-        K = K[mask]
-        mu, features_reduced = suboptimal_median(features_thresholded, K)
-        distances, predictions, objective = evaluator.update('th 0.5', features_subset, mu, bag_to_index)
+            S = 1 - S[len(bag_trn):]
+            P_tst = P[len(bag_trn):]
 
-        threshold = 0.75
-        mask = scores_subset >= threshold
-        features_thresholded = features_subset[mask]
-        K = np.array(list(index_to_bag.values()))
-        K = K[mask]
-        mu, features_reduced = suboptimal_median(features_thresholded, K)
-        distances, predictions, objective = evaluator.update('th 0.75', features_subset, mu, bag_to_index)
+            P_trn = P[:len(bag_trn)].astype(bool)
+            
+            mu = median(features_trn_subset[P_trn])
+            
+            _, predictions, _ = evaluator.update('bayes median', features_tst_subset, mu, bag_to_index_tst)
+            
+            if save_features:
+                add_features(predictions, ijbb_features_bayes_median)
 
-        threshold = 0.95
-        mask = scores_subset >= threshold
-        features_thresholded = features_subset[mask]
-        K = np.array(list(index_to_bag.values()))
-        K = K[mask]
-        mu, features_reduced = suboptimal_median(features_thresholded, K)
-        distances, predictions, objective = evaluator.update('th 0.95', features_subset, mu, bag_to_index)
-        # '''
+            bayes_distances.extend(S)
+            bayes_predictions.extend(P_tst)
 
-        #! Different PCA evaluation
-        '''
-        mu, features_reduced = suboptimal_median(features_subset, list(index_to_bag.values()), 2)
-        distances, predictions, objective = evaluator.update('pca {2}С 2D', features_reduced, mu, bag_to_index)
+        #! Optimal MILP median
+        if eval_milp_median:
+            mu, features_reduced = optimal_median(features_trn_subset, list(index_to_bag_trn.values()))
+            evaluator.update('milp median', features_reduced, mu, bag_to_index_tst)
 
-        mu, features_reduced = adaptive_suboptimal_median(features_subset, list(index_to_bag.values()), bag_to_index, 2)
-        distances, predictions, objective = evaluator.update('pca {2,4}C 2D', features_reduced, mu, bag_to_index)
+        #! Bag median
+        if eval_bag_median:
+            mu = suboptimal_median(features_trn_subset, list(index_to_bag_trn.values()))
+            distances, predictions, objective = evaluator.update('bag median', features_tst_subset, mu, bag_to_index_tst)
+            if save_features:
+                add_features(predictions, ijbb_features_bag_median)
 
-        mu, features_reduced = adaptive_suboptimal_median(features_subset, list(index_to_bag.values()), bag_to_index)
-        distances, predictions, objective = evaluator.update('pca {2,4}C 256D', features_reduced, mu, bag_to_index)
-        # '''
+    if eval_bayes_median:
+        evaluator.distances['bayes median'] = bayes_distances
+        evaluator.predictions['bayes median'] = bayes_predictions
 
-    #! Generate plots
+    if save_features:
+        np.save('skipped_subjects.npy', np.array(skipped_subjects))
+        np.save('ijbb_features_optimal.npy', ijbb_features_optimal)
+        np.save('ijbb_features_median.npy', ijbb_features_median)
+        np.save('ijbb_features_bag_median.npy', ijbb_features_bag_median)
+        np.save('ijbb_features_bayes_median.npy', ijbb_features_bayes_median)
+        np.save('ijbb_features_iterative_median.npy', ijbb_features_iterative_median)
 
-    for method in evaluator.get_methods():
-        if len(evaluator.predictions[method]) == 0:
-            continue
-        
-        objective = np.mean(evaluator.objectives[method])
-        label = method.replace('_', ' ')
-        label += f': {objective:.2f}'
-
-        recall, prec = calc_prec_recall(evaluator.distances[method], evaluator.true, evaluator.predictions[method])
-        plt.plot(recall, prec, label=label)
-
-        print(f'{label}')
-
-    plot_prec_recall()
+    evaluator.plot_prec_recall()
